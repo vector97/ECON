85746aca85b88580635009e7377ca664212f71d1
Viktor Bogatenkov <vector_97@mail.ru>
2022-01-31 22:28:00 +0400

    Внесены изменения в проеÑкт

--- js/script.js
+++ js/script.js
@@ -84,3 +84,54 @@ for (let i = 0; i < showMore.length; i++) {
     }
   });
 }
+
+// слайдер цены
+
+$(function() {
+
+  // Initiate Slider
+  $('#slider-range').slider({
+    range: true,
+    min: 3000,
+    max: 30000,
+    step: 100,
+    values: [3000, 30000]
+  });
+
+  // Move the range wrapper into the generated divs
+  $('.ui-slider-range').append($('.range-wrapper'));
+
+  // Apply initial values to the range container
+  $('.range').html('<span class="range-value"><sup>₽</sup>' + $('#slider-range').slider("values", 0).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + '</span><span class="range-divider"></span><span class="range-value"><sup>₽</sup>' + $("#slider-range").slider("values", 1).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + '</span>');
+
+
+
+
+  // Rotate the gears
+  var gearOneAngle = 0,
+    gearTwoAngle = 0,
+    rangeWidth = $('.ui-slider-range').css('width');
+
+
+  $('#slider-range').slider({
+    slide: function(event, ui) {
+
+      // Update the range container values upon sliding
+
+      $('.range').html('<span class="range-value"><sup>₽</sup>' + ui.values[0].toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + '</span><span class="range-divider"></span><span class="range-value"><sup>₽</sup>' + ui.values[1].toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + '</span>');
+
+      // Get old value
+      var previousVal = parseInt($(this).data('value'));
+
+      // Save new value
+      $(this).data({
+        'value': parseInt(ui.value)
+      });
+
+      }
+
+  });
+
+
+
+});
